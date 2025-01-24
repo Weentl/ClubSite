8cd2731f7271b49e1986324477c1b34ef6d76147
Tu Nombre <tuemail@example.com>
2025-01-23 20:00:41 -0600

    cors

--- server.js
+++ server.js
@@ -1,24 +1,29 @@
 const express = require('express');
-const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
 const authRoutes = require('./routes/authRoutes');
 
-dotenv.config(); // Cargar variables de entorno
+dotenv.config(); // Cargar las variables de entorno desde el archivo .env
+
 const app = express();
-app.use(express.json()); // Para parsear JSON en las solicitudes
 
-// Conectar a MongoDB Atlas
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+// Middleware
+app.use(cors());
+app.use(express.json()); // Permite manejar las solicitudes con cuerpo JSON
+
+// Conectar a MongoDB
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
-  .catch((error) => console.log('MongoDB connection error:', error));
+  .catch(err => console.log(err));
 
-app.use('/api/auth', authRoutes); // Rutas de autenticación
+// Rutas
+app.use('/auth', authRoutes); // Ruta para autenticación
 
+// Iniciar servidor
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
 
